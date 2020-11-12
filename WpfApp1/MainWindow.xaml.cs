733c74bbc60b38ab4a61663aff859872c059e824
tamar <tamitamtam9@gmail.com>
2020-11-12 20:06:09 +0200

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RedLightButtom_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("BREAKES");
+        }
+
+        private void GreenLightButton_Click(object sender, RoutedEventArgs e)
+        {
+             GreenCar.Margin = new Thickness(100, 122.8, 0, 0);
+        }
+    }
+}
